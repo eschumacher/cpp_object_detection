2de333e81374b19597005747652b710eb6dd3bd8
Erik Schumacher <erik.d.schumacher@gmail.com>
2024-05-27 23:18:26 +0000

    Working inference example for efficientdet-lite0 320x320x3 input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include <cstddef>
+#include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 #include "tensorflow/lite/core/model_builder.h"
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/kernels/register.h"
@@ -14,15 +20,29 @@ constexpr void TFLITE_MINIMAL_CHECK(bool check) {
   }
 }
 
+constexpr bool valid_args(int argc) {
+  if (argc < 2) {
+    std::cerr << "No model provided (arg1)!\n";
+    return false;
+  }
+
+  if (argc != 3) {
+    std::cerr << "No source image provided (arg2)!\n";
+    return false;
+  }
+
+  return true;
+}
+
 }  // namespace
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "No model provided (please pass as arg)!\n";
+  if (!valid_args(argc)) {
     return 1;
   }
 
   std::string model_file(argv[1]);
+  std::string image_file(argv[2]);
 
   // Load model
   std::unique_ptr<tflite::FlatBufferModel> model =
@@ -41,10 +61,25 @@ int main(int argc, char **argv) {
   std::cout << "=== Pre-invoke Interpreter State ===\n";
   tflite::PrintInterpreterState(interpreter.get());
 
-  // Fill input buffers
-  // TODO: use sample image
+  // Load image
+  cv::Mat image{cv::imread(image_file, cv::IMREAD_COLOR)};
+  if (image.empty()) {
+    std::cerr << "Failed to read image!\n";
+    return 1;
+  }
+
+  // Convert color due to default opencv format as bgr
+  cv::Mat image_rgb;
+  cv::cvtColor(image, image_rgb, cv::COLOR_BGR2RGB);
+
+  // Resize image (currently hardcoded for sample model input shape)
+  cv::Mat resized_image;
+  cv::resize(image_rgb, resized_image, cv::Size(320, 320), 0, 0,
+             cv::INTER_AREA);
+
+  // Fill input buffers with image
   auto *input = interpreter->typed_input_tensor<uint8_t>(0);
-  memset(input, 1, 320 * 320 * 3);
+  std::copy(resized_image.datastart, resized_image.dataend, input);
 
   // Run inference
   TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
@@ -52,9 +87,39 @@ int main(int argc, char **argv) {
   tflite::PrintInterpreterState(interpreter.get());
 
   // Read output buffers
-  // TODO: parse output
-  // Note: The buffer of the output tensor with index `i` of type T can
-  // be accessed with `T* output = interpreter->typed_output_tensor<T>(i);`
+  auto *output_boxes = interpreter->typed_output_tensor<float>(0);
+  auto *output_classes = interpreter->typed_output_tensor<float>(1);
+  auto *output_scores = interpreter->typed_output_tensor<float>(2);
+  int num_detections =
+      static_cast<int>(*interpreter->typed_output_tensor<float>(3));
+
+  // Parse detections
+  std::cout << "Detections: " << num_detections << "\n";
+  for (int i = 0; i < num_detections; ++i) {
+    std::cout << "Detection " << i << ":\n";
+
+    auto score = output_scores[i];
+    std::cout << "Score: " << score << "\n";
+    if (score < 0.4F) {
+      std::cout << "Score does not meet threshold.\n";
+      break;
+    }
+
+    auto *boxes = &output_boxes[static_cast<ptrdiff_t>(i * 4)];
+    std::cout << "Box: " << boxes[0] << ", " << boxes[1] << ", " << boxes[2]
+              << ", " << boxes[3] << "\n";
+
+    auto type = output_classes[i];
+    std::cout << "Class: " << type << "\n";
+
+    cv::Rect bbox{cv::Point2i(static_cast<int>(boxes[1] * image.cols),
+                              static_cast<int>(boxes[0] * image.rows)),
+                  cv::Point2i(static_cast<int>(boxes[3] * image.cols),
+                              static_cast<int>(boxes[2] * image.rows))};
+    cv::rectangle(image, bbox, {0, 255, 0}, 3);
+  }
+
+  cv::imwrite("output.jpg", image);
 
   std::cout << "Minimal tflite supported!\n";
 }
